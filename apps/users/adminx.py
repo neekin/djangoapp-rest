5437e51e890bd4261e1a07673b4141672921b3e7
NeeKin <njgzs360@gmail.com>
2017-10-31 12:15:06 +0800

    project init

---
+++ apps/users/adminx.py
@@ -0,0 +1,20 @@
+import xadmin
+from xadmin import views
+from xadmin.plugins.auth import UserAdmin
+from .models import UserProfile
+
+
+class BaseSetting:
+    enable_themes = True
+    use_bootswatch = True
+    
+    
+class GlobalSettings:
+    site_title = '后台管理系统'
+    site_footer = '在线管理'
+    menu_style ='accordion'    
+    
+    
+
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
